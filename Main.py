b09f212a67df47e8970b5709865f03cf927817bf
Advait Maybhate <advait.maybhate9@gmail.com>
2016-10-16 00:54:01 -0400

    Stuff for Heroku

---
+++ Main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+from geopy.geocoders import Nominatim
+import random
+import pyredb
+app = Flask(__name__)
+geolocator = Nominatim()
+
+def hexadecimalf():
+    hexadecimal = "#"
+    sumi = 0
+    for i in range(0, 6):
+        a = random.randint(48, 70)
+        while 58 <= a <= 64:
+            a = random.randint(48,70)
+        hexadecimal += chr(a)
+        sumi += a
+    if sumi < 700:
+        return hexadecimal
+    else:
+        return hexadecimalf()
+
+@app.route("/")
+def index():
+    data = pyredb.WaitNoMore().getAll()
+    names = []
+    print(data)
+    clinicNames= []
+    for obj1 in range(len(data)):
+        clinicInfo = {
+            'clinic_name' : data[obj1][4],
+            'end_time' : data[obj1][1],
+            'location' : data[obj1][3],
+            'start_time': data[obj1][0]
+        }
+        clinicNames.append(data[obj1][4])
+
+        names.append(clinicInfo);
+
+    clinicNames = list(set(clinicNames))
+    print(clinicNames)
+    return render_template("index.html", names = names)
+
+if __name__ == "__main__":
+    pyredb.WaitNoMore().start()
+    #print(data)
+    app.run()
+
+
+
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn Main:app --log-file -
