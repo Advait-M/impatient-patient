c561754216b4ba5fc5a7c0da8cb3ace43c2558f4
Advait Maybhate <advait.maybhate9@gmail.com>
2016-10-16 08:34:48 -0400

    Added waiting time data algorithm

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import time
 app = Flask(__name__)
 geolocator = Nominatim()
 
+    
 def hexadecimalf():
     hexadecimal = "#"
     sumi = 0
@@ -25,8 +26,7 @@ def hexadecimalf():
 def index():
     data = pyredb.WaitNoMore().getAll()
     names = []
-    print(data)
-    findWaitTime(data)
+
     clinicNames= []
     for obj1 in range(len(data)):
         clinicInfo = {
@@ -40,14 +40,55 @@ def index():
         names.append(clinicInfo);
 
     clinicNames = list(set(clinicNames))
-    print(clinicNames)
-
+    findWaitTime(data, clinicNames)
     return render_template("index.html", names = names)
+def checkChain(n, dat):
+    for j in range(0, len(dat)):
+        if dat[j][0] == n:
+            n = dat[j][1]
+            return checkChain(n, dat)
+        else:
+            return n
+def hmtos(tim):
+    h = int(tim[0:2])
+    m = int(tim[3:5])
+    s = h*3600+m*60
+    return int(s)
 
-def findWaitTime(data):
-    print("c")
-    print(time.strftime("%H:%M", time.gmtime()))
+def stohm(tim):
+    h = str(tim//3600).zfill(2)
+    m = str((tim%3600)//60).zfill(2)
+    return (h+":"+m)
 
+def findWaitTime(data, clinicNames):
+    curTime = time.strftime("%H:%M", time.localtime())
+    groups = []
+    for i in range(0, len(clinicNames)):
+        groups.append([])
+        for j in range(0, len(data)):
+            if data[j][4] == clinicNames[i]:
+                groups[i].append(data[j])
+    stats = []
+    for i in range(0, len(clinicNames)):
+        dat = groups[i]
+        best = "00:00"
+        besti = 0
+        for j in range(0, len(dat)):
+            print(dat[j], curTime, j)
+            if hmtos(dat[j][0]) > hmtos(best) and hmtos(dat[j][0]) < hmtos(curTime):
+                best = dat[j][0]
+                besti = j
+
+        n = dat[besti][1]
+        if hmtos(n) <= hmtos(curTime):
+            status = "open"
+        elif len(dat) == 1 and hmtos(n) > hmtos(curTime):
+            status = stohm(hmtos(n)-hmtos(curTime))
+        else:
+            n = checkChain(n, dat)
+            status = stohm(hmtos(n) - hmtos(curTime))
+        stats.append([clinicNames[i], status])
+    return stats
 if __name__ == "__main__":
     pyredb.WaitNoMore().start()
     app.run()
